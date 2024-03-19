4568cabc7f126058148b463e4a4ec5dbf6f85a3c
kimsugyeom05 <163097362+kimsugyeom05@users.noreply.github.com>
2024-03-19 14:12:46 +0900

    Add files via upload

---
+++ 202444035/ex03-07.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    system("title 정수형 테이터의 자릿수 지정 형식");
+
+    printf("|%d|",358);
+    printf(":정수형 기본 자릿수\n");
+
+    printf("|%d|",358);
+    printf(":전체 자릿수 5개\n");
+
+    printf("|%d|",358);
+    printf(":전체 자릿수는 5개이고 빈칸은 0으롷 채움\n");
+    return 0;
+}
